237fb1ac7b2eecdc18c3e08075d6b8571498f589
zohaibyounas <zohaiby737@gmail.com>
2024-11-11 10:13:26 +0500

    updated

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import HeadlineCards from './components/HeadlineCards'
+import Food from './components/Food'
+import Category from './components/Category'
+import Footer from './components/Footer'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <HeadlineCards />
+      <Food />
+      <Category />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
